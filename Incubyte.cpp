23686f6d0e532e0a1305a427a372f42e7ef9ef0d
Heet Senghani <91259688+heet2312@users.noreply.github.com>
2023-08-29 23:33:09 +0530

    function are define

--- Incubyte.cpp
+++ Incubyte.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
 
+// inverse of direction
+char inverse(char direction)
+{
+    switch (direction)
+    {
+    case 'N':
+        return 'S';
+        break;
+    case 'S':
+        return 'N';
+        break;
+    case 'E':
+        return 'W';
+        break;
+    case 'W':
+        return 'E';
+        break;
+    case 'U':
+        return 'D';
+        break;
+    case 'D':
+        return 'U';
+        break;
+    }
+}
+
 // for mooving forward
 void forward(int cordinate[3], char direction)
 {
@@ -9,18 +35,23 @@ void forward(int cordinate[3], char direction)
     case 'N':
         cordinate[1] += 1;
         break;
+
     case 'S':
         cordinate[1] -= 1;
         break;
+
     case 'E':
         cordinate[0] += 1;
         break;
+
     case 'W':
         cordinate[0] -= 1;
         break;
+
     case 'U':
         cordinate[2] += 1;
         break;
+
     case 'D':
         cordinate[2] -= 1;
         break;
@@ -35,18 +66,23 @@ void backward(int cordinate[3], char direction)
     case 'N':
         cordinate[1] -= 1;
         break;
+
     case 'S':
         cordinate[1] += 1;
         break;
+
     case 'E':
         cordinate[0] -= 1;
         break;
+
     case 'W':
         cordinate[0] += 1;
         break;
+
     case 'U':
         cordinate[2] -= 1;
         break;
+
     case 'D':
         cordinate[2] += 1;
         break;
@@ -54,40 +90,195 @@ void backward(int cordinate[3], char direction)
 }
 
 // left rotation
-void left(char &direction)
+void left(char &direction, char &prev)
 {
     switch (direction)
     {
     case 'N':
         direction = 'W';
         break;
+
     case 'S':
         direction = 'E';
         break;
+
     case 'E':
         direction = 'N';
         break;
+
     case 'W':
         direction = 'S';
+        break;
+
+    case 'U':
+        switch (prev)
+        {
+        case 'E':
+            direction = 'N';
+            break;
+
+        case 'W':
+            direction = 'S';
+            break;
+
+        case 'N':
+            direction = 'W';
+            break;
+
+        case 'S':
+            direction = 'E';
+            break;
+        }
+        break;
+
+    case 'D':
+        switch (prev)
+        {
+        case 'E':
+            direction = 'S';
+            break;
+
+        case 'W':
+            direction = 'N';
+            break;
+
+        case 'N':
+            direction = 'E';
+            break;
+
+        case 'S':
+            direction = 'W';
+            break;
+        }
         break;
     }
 }
 
 // right rotation
-void right(char &direction)
+void right(char &direction, char &prev)
 {
     switch (direction)
     {
     case 'N':
         direction = 'E';
         break;
+
     case 'S':
         direction = 'W';
         break;
+
     case 'E':
         direction = 'S';
         break;
+
     case 'W':
+        direction = 'N';
+        break;
+
+    case 'U':
+        switch (prev)
+        {
+        case 'E':
+            direction = 'S';
+            break;
+
+        case 'W':
+            direction = 'N';
+            break;
+
+        case 'N':
+            direction = 'E';
+            break;
+
+        case 'S':
+            direction = 'W';
+            break;
+        }
+        break;
+
+    case 'D':
+        switch (prev)
+        {
+        case 'E':
+            direction = 'N';
+            break;
+
+        case 'W':
+            direction = 'S';
+            break;
+
+        case 'N':
+            direction = 'W';
+            break;
+
+        case 'S':
+            direction = 'E';
+            break;
+        }
+        break;
+    }
+}
+
+// upward rotation
+void up(char &direction, char &prev)
+{
+    switch (direction)
+    {
+    case 'N':
+        prev = 'N';
+        direction = 'U';
+        break;
+
+    case 'S':
+        prev = 'S';
+        direction = 'D';
+        break;
+
+    case 'E':
+        prev = 'E';
+        direction = 'U';
+        break;
+
+    case 'W':
+        prev = 'W';
+        direction = 'D';
+        break;
+
+    case 'U':
+        direction = inverse(prev);
+        break;
+
+    case 'D':
+        direction = inverse(prev);
+        break;
+    }
+}
+
+// downward rotation
+void down(char &direction, char &prev)
+{
+    switch (direction)
+    {
+    case 'N':
+        prev = 'N';
+        direction = 'D';
+        break;
+    case 'S':
+        prev = 'S';
+        direction = 'U';
+        break;
+    case 'E':
+        prev = 'E';
+        direction = 'D';
+        break;
+    case 'W':
+        prev = 'W';
+        direction = 'U';
+        break;
+    case 'U':
+        direction = 'S';
+        break;
+    case 'D':
         direction = 'N';
         break;
     }
@@ -95,13 +286,17 @@ void right(char &direction)
 
 int main()
 {
+    // taking input
     string s;
     cout << "Enter command:";
     cin >> s;
+
+    // initial cordinates
     char direction = 'N';
-    char prev = 'N';
+    char prev;
     int cordinate[3] = {0, 0, 0};
 
+    // traversing the string
     for (int i = 0; i < s.length(); i++)
     {
         if (s[i] == 'F')
@@ -112,7 +307,29 @@ int main()
         {
             backward(cordinate, direction);
         }
+        else if (s[i] == 'L')
+        {
+            left(direction, prev);
+        }
+        else if (s[i] == 'R')
+        {
+            right(direction, prev);
+        }
+        else if (s[i] == 'U')
+        {
+            up(direction, prev);
+        }
+        else if (s[i] == 'D')
+        {
+            down(direction, prev);
+        }
+        else
+        {
+            cout << "Invalid command" << endl;
+            break;
+        }
     }
     cout << "Final cordinates are: " << cordinate[0] << " " << cordinate[1] << " " << cordinate[2] << endl;
+    cout << "Final direction is: " << direction << endl;
     return 0;
 }
